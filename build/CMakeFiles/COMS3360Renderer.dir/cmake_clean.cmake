6884a44fa42f3e3f247816efee7b26d8ffc159d5
AdamT-TLee <adam832lee@gmail.com>
2025-08-29 18:14:26 -0500

    Moved Everything to a repo and finished everything before making a sphere

---
+++ build/CMakeFiles/COMS3360Renderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMS3360Renderer.dir/main.cpp.o"
+  "CMakeFiles/COMS3360Renderer.dir/main.cpp.o.d"
+  "COMS3360Renderer"
+  "COMS3360Renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COMS3360Renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/COMS3360Renderer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for COMS3360Renderer.
+# This may be replaced when dependencies are built.
