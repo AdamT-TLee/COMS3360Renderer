b46b82a9d6587be5db963d8a692b02471133872a
AdamT-TLee <adam832lee@gmail.com>
2025-12-07 00:00:19 -0600

    finished book 2 of ray tracing in weekend

--- build/CMakeFiles/COMS3360Renderer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for COMS3360Renderer.
-# This may be replaced when dependencies are built.
--- quad.h
+++ quad.h
@@ -2,6 +2,7 @@
 #define QUAD_H
 
 #include "hittable.h"
+#include "hittable_list.h"
 #include "vec3.h"
 
 class quad : public hittable {
@@ -81,6 +82,28 @@ private:
     double D;
     vec3 w;
 };
+
+inline shared_ptr<hittable_list> box(const point3& a, const point3& b, shared_ptr<material> mat) {
+    auto sides = make_shared<hittable_list>();
+
+    // Construct the two opposite vertices with the minimum and maximum coordinates.
+    point3 min = point3(std::fmin(a.x(), b.x()), std::fmin(a.y(), b.y()), std::fmin(a.z(), b.z()));
+    point3 max = point3(std::fmax(a.x(), b.x()), std::fmax(a.y(), b.y()), std::fmax(a.z(), b.z()));
+
+    vec3 dx = vec3(max.x() - min.x(), 0 ,0);
+    vec3 dy = vec3(0, max.y() - min.y() ,0);
+    vec3 dz = vec3(0, 0 , max.z() - min.z());
+
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), max.z()),  dx,  dy, mat)); //front
+    sides->add(make_shared<quad>(point3(max.x(), min.y(), min.z()),  -dx,  dy, mat)); //back
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()),  dz,  dy, mat)); //left
+    sides->add(make_shared<quad>(point3(max.x(), min.y(), max.z()),  -dz,  dy, mat)); //right
+    sides->add(make_shared<quad>(point3(min.x(), max.y(), max.z()),  dx,  -dz, mat)); //top
+    sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()),  dx,  dz, mat)); //bottom
+
+
+    return sides;
+}
 
 class triangle : public quad {
 public:
